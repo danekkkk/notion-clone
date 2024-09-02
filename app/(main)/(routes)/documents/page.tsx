eeb9f661e2ddd753010b5961b1d234c559c40392
danekkkk <101827298+danekkkk@users.noreply.github.com>
2024-09-02 17:04:10 +0200

    sidebar user dropdown

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,35 @@
+"use client";
+
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { PlusCircleIcon } from "lucide-react";
+
 const DocumentsPatge = () => {
-  return <div>
-    Navigation page
-  </div>;
+  const { user } = useUser();
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="dark:hidden"
+      />
+      <Image
+        src="/empty-dark.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="hidden dark:block"
+      />
+      {user && user.firstName && <h2>Welcome to {user.firstName}&apos;s Notion</h2>}
+      <Button>
+        <PlusCircleIcon className="size-4 mr-2" />
+        Create a note
+      </Button>
+    </div>
+  );
 };
 
 export default DocumentsPatge;
