f4da21282386e0c0ae01259ede2a46433313cddc
danekkkk <101827298+danekkkk@users.noreply.github.com>
2024-09-04 15:16:30 +0200

    schema & api

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -4,9 +4,27 @@ import Image from "next/image";
 import { useUser } from "@clerk/clerk-react";
 import { Button } from "@/components/ui/button";
 import { PlusCircleIcon } from "lucide-react";
+import { useMutation } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import {toast} from "sonner"
 
 const DocumentsPatge = () => {
   const { user } = useUser();
+  const create = useMutation(api.documents.create);
+
+  const onCreate = () => {
+    const promise = create({
+      title: "Untitled"
+    })
+
+    toast.promise(promise, {
+      loading: "Creating a new note...",
+      success: "New note created!",
+      error: "Failed to create a new note."
+    })
+
+  }
+
   return (
     <div className="h-full flex flex-col items-center justify-center space-y-4">
       <Image
@@ -24,7 +42,7 @@ const DocumentsPatge = () => {
         className="hidden dark:block"
       />
       {user && user.firstName && <h2>Welcome to {user.firstName}&apos;s Notion</h2>}
-      <Button>
+      <Button onClick={onCreate}>
         <PlusCircleIcon className="size-4 mr-2" />
         Create a note
       </Button>
